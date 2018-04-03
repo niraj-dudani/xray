a0fd88dfa5421e1e1796d725f1eef1efbee5c93c
Niraj Dudani <niraj@locus.sh>
2018-04-03 12:08:30 +0530

    Refactoring: splitting into functions.

--- xray.cc
+++ xray.cc
@@ -1,28 +1,46 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	float x = 1.0;
-	
-	char* x_address = (char*) &x;
-	int n_bytes = sizeof(x);
+vector<int> read_bits(float value) {
+	char* value_address = (char*) &value;
+	int n_bytes = sizeof(value);
+	vector<int> bits;
 	
 	for (int i_byte = 0; i_byte < n_bytes; i_byte++) {
-		char* byte_address = x_address + i_byte;
+		char* byte_address = value_address + i_byte;
 		char byte = *byte_address;
-		
-		cout << i_byte << " ";
 		
 		for (int i_bit = 7; i_bit >= 0; i_bit--) {
 			unsigned char mask = 1 << i_bit;
 			unsigned char masked = byte & mask;
-			unsigned char bit = masked >> i_bit;
+			unsigned char shifted = masked >> i_bit;
+			int bit = static_cast<int>(shifted);
 			
-			//~ cout << (int) mask << " " << (int) masked << " " << (int) bit << endl;
-			
-			cout << (int) bit;
+			bits.push_back(bit);
 		}
-		cout << endl;
 	}
+	
+	return bits;
+}
+
+void show_bits(vector<int> bits) {
+	for(int i = 0; i < bits.size(); i++) {
+		cout << bits[i];
+		
+		if((i + 1) % 8 == 0) {
+			cout << endl;
+		}
+	}
+}
+
+int main() {
+	float x = 1.0;
+	
+	vector<int> bits = read_bits(x);
+	
+	show_bits(bits);
+	
+	return 0;
 }
