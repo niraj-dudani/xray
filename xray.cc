5cad7ab1d867ee35e1469177d3eaa7cf2a6c2bfc
Niraj Dudani <niraj@locus.sh>
2018-04-03 12:25:06 +0530

    Storing bits in vectors of size 8, instead of as a single vector.

--- xray.cc
+++ xray.cc
@@ -4,15 +4,16 @@
 using namespace std;
 
 template <typename T>
-vector<int> read_bits(T value) {
+vector<vector<int>> read_bits(T value) {
 	char* value_address = (char*) &value;
 	int n_bytes = sizeof(value);
-	vector<int> bits;
+	vector<vector<int>> bytes;
 	
 	for (int i_byte = 0; i_byte < n_bytes; i_byte++) {
 		char* byte_address = value_address + i_byte;
 		char byte = *byte_address;
 		
+		vector<int> bits;
 		for (int i_bit = 7; i_bit >= 0; i_bit--) {
 			unsigned char mask = 1 << i_bit;
 			unsigned char masked = byte & mask;
@@ -21,27 +22,37 @@ vector<int> read_bits(T value) {
 			
 			bits.push_back(bit);
 		}
+		
+		bytes.push_back(bits);
 	}
 	
-	return bits;
+	return bytes;
 }
 
-void show_bits(vector<int> bits) {
-	for(int i = 0; i < bits.size(); i++) {
-		cout << bits[i];
-		
-		if((i + 1) % 8 == 0) {
-			cout << endl;
+void show_bits(vector<vector<int>> bytes) {
+	for(
+		vector<vector<int>>::iterator i_byte = bytes.begin();
+		i_byte != bytes.end();
+		i_byte++
+	) {
+		for(
+			vector<int>::iterator i_bit = i_byte->begin();
+			i_bit != i_byte->end();
+			i_bit++
+		) {
+			cout << *i_bit;
 		}
+		
+		cout << endl;
 	}
 }
 
 int main() {
 	float x = 1.0;
 	
-	vector<int> bits = read_bits(x);
+	vector<vector<int>> bytes = read_bits(x);
 	
-	show_bits(bits);
+	show_bits(bytes);
 	
 	return 0;
 }
